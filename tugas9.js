637d61da498c79c9f5ec85558280925b5bb7792b
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 08:55:56 +0700

    TUGAS 9 JS

---
+++ tugas9.js
@@ -0,0 +1,12 @@
+function tugas9() {
+    var dataDiri = {
+        nama: "Ridwan Aditya Saputra",
+        umur: 24,
+        pendidikanTerakhir: "SMA",
+    }
+    for (var a in dataDiri) {
+        console.log(dataDiri[a]);
+    }
+
+}
+tugas9();
